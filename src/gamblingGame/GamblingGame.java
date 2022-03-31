2cff44b6b9437dcff8733c209a0e90d701cedbb8
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-03-31 16:10:56 +0530

    Win and loose added

--- src/gamblingGame/GamblingGame.java
+++ src/gamblingGame/GamblingGame.java
@@ -1,19 +1,29 @@
 package gamblingGame;
-
+import java.util.Random;
 public class GamblingGame {
-
-	public static void main(String[] args) {
-		int STAKE = 100;
-		int BET =  1;
-		System.out.println("Welcome To Gambling Simulator");
-		System.out.println("Gambler start with stake of $" + STAKE + " every day");
-		System.out.println("Gambler bet for $" + BET + " every game");
+	public static final int STAKE_PER_DAY = 100;
+	public static final int BET_PER_GAME = 1;
+	public static final int WIN = 1;
+	public static int stake = 0;
+	public static void winCheck() {
+		Random random = new Random();
+		int betReturns = random.nextInt(2);
+		if (betReturns == WIN) {
+			stake++;
+			System.out.println("win");
+		} else {
+			stake--;
+			System.out.println("loss");
+		}
+		System.out.println("stake = " + stake);
 	}
-
-
-
+		public static void main(String[] args) {
+			System.out.println("Welcome to Gambling Simulation");
+			winCheck();
+		
 
 	
+
 	}
-
-
+		
+	}
