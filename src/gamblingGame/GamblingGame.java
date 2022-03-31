282f807f04c3dd02592b89c2820b89a855011a6e
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-03-31 17:33:47 +0530

    win or loose 50% stake

--- src/gamblingGame/GamblingGame.java
+++ src/gamblingGame/GamblingGame.java
@@ -1,29 +1,44 @@
 package gamblingGame;
-import java.util.Random;
+import java.util.Scanner;
 public class GamblingGame {
-	public static final int STAKE_PER_DAY = 100;
-	public static final int BET_PER_GAME = 1;
-	public static final int WIN = 1;
-	public static int stake = 0;
-	public static void winCheck() {
-		Random random = new Random();
-		int betReturns = random.nextInt(2);
-		if (betReturns == WIN) {
-			stake++;
-			System.out.println("win");
-		} else {
-			stake--;
-			System.out.println("loss");
-		}
-		System.out.println("stake = " + stake);
+	static int AMOUNT_OF_STAKE=100;
+	static int BET_AMOUNT=1;
+	static int WON=1;
+	static int LOST=0;
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Starting amount of stake is "+AMOUNT_OF_STAKE);
+		 System.out.println("Amount to bet after every play "+BET_AMOUNT);
+		 int cashInHand = AMOUNT_OF_STAKE;
+		 while(cashInHand > 50 && cashInHand < 150) {
+			 int play = (int) Math.floor(Math.random() * 10) % 2;
+			 System.out.println("Gamblers play:"+play);
+		 
+			 switch(play) {
+		 		case 1:
+		 			cashInHand = cashInHand + BET_AMOUNT;
+		 			System.out.println("Total current cash gambler has "+cashInHand);
+		 			break;
+		 		case 0:
+		 			cashInHand = cashInHand - BET_AMOUNT;
+		 			System.out.println("Total current cash gambler has "+cashInHand);
+		 			break;
+		 		default:
+		 			System.out.println("default");
+			 }
+			 if(play == 1) {
+				 System.out.println("Player won the current play");
+			 }
+			 else {
+				 System.out.println("Player lost the current play");
+			 }
+		 
+		 }
+		 if(cashInHand==150 || cashInHand==50) {
+			 System.out.println("--------------------------------------------");
+			 System.out.println("Player would like to resign for current day");
+			 System.out.println("--------------------------------------------");
+		 }
 	}
-		public static void main(String[] args) {
-			System.out.println("Welcome to Gambling Simulation");
-			winCheck();
-		
 
-	
-
-	}
-		
-	}
+}
