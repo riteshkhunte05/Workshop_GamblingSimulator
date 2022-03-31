81b5d9f99cbeefeff8edfa30e4c94470e3b2c1fe
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-03-31 15:53:55 +0530

    Gambling Simulator added

---
+++ src/gamblingGame/GamblingGame.java
@@ -0,0 +1,19 @@
+package gamblingGame;
+
+public class GamblingGame {
+
+	public static void main(String[] args) {
+		int STAKE = 100;
+		int BET =  1;
+		System.out.println("Welcome To Gambling Simulator");
+		System.out.println("Gambler start with stake of $" + STAKE + " every day");
+		System.out.println("Gambler bet for $" + BET + " every game");
+	}
+
+
+
+
+	
+	}
+
+
